d815f89343a737a8cdd5b0a6a83f6d2469b66bf2
Jae Wook Lee <jwleemd@gmail.com>
2015-10-16 15:00:53 +0900

    assignment
    The Week 2 Assignment for Getting and Cleaning Data

--- run_analysis.R
+++ run_analysis.R
@@ -1,73 +1,44 @@
-### Merges the training and the test sets to create one data set.
-
-# setting the working directory
-
-setwd("C:/Users/jae/Documents/datasciencecoursera/")
-
 # downloading the data set using URL
 
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url, destfile="Dataset.zip")
 unzip("Dataset.zip")
 
-# reading in label data
-
-activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt", header=F)
-feature.labels <- read.table("UCI HAR Dataset/features.txt", header=F)
-
-# reading in the train set
+### Merges the training and the test sets to create one data set.
 
 train.set <- read.table("UCI HAR Dataset/train/X_train.txt", header=F)
-train.activity <- read.table("UCI HAR Dataset/train/y_train.txt", header=F)
-train.subject <- read.table("UCI HAR Dataset/train/subject_train.txt", header=F)
-
-# assign column names and add subject and activity data 
-
-colnames(train.set) <- feature.labels$V2
-train.set$subject <- train.subject$V1
-train.set$activity <- train.activity$V1
-train.set$origin <- "train"
-
-# rearranging the order of columns
-
-train.set <- train.set[, c(ncol(train.set)-2, ncol(train.set)-1, ncol(train.set), 1:(ncol(train.set)-3))]
-
-# reading in the test set
-
 test.set <- read.table("UCI HAR Dataset/test/X_test.txt", header=F)
-test.activity <- read.table("UCI HAR Dataset/test/y_test.txt", header=F)
-test.subject <- read.table("UCI HAR Dataset/test/subject_test.txt", header=F)
-
-# assign column names and add subject and activity data
-
-colnames(test.set) <- feature.labels$V2
-test.set$subject <- test.subject$V1
-test.set$activity <- test.activity$V1
-test.set$origin <- "test"
-
-
-# rearranging the order of columns
-
-test.set <- test.set[, c(ncol(test.set)-2, ncol(test.set)-1, ncol(test.set), 1:(ncol(test.set)-3))]
-
-# merging the train and test datasets into one dataset
-
 total.set <- rbind(train.set, test.set)
 
 ###Extracts only the measurements on the mean and standard deviation for each measurement. 
 
-mean_idx <- grep("mean()", colnames(total.set))
-std_idx <- grep("std()", colnames(total.set))
-mean_and_std <- total.set[, c(1,2,3,mean_idx, std_idx)]
+feature.labels <- read.table("UCI HAR Dataset/features.txt", header=F, stringsAsFactors=F)
+feature.labels <- feature.labels$V2
 
-#Uses descriptive activity names to name the activities in the data set
+mean_idx <- grep("mean()", feature.labels)
+std_idx <- grep("std()", feature.labels)
+mean_and_std_data <- total.set[, c(mean_idx, std_idx)]
 
+### Uses descriptive activity names to name the activities in the data set
 
+activity.labels <- read.table("UCI HAR Dataset/activity_labels.txt", header=F, stringsAsFactors=F)
+train.activity <- read.table("UCI HAR Dataset/train/y_train.txt", header=F, stringsAsFactors = F)$V1
+test.activity <- read.table("UCI HAR Dataset/test/y_test.txt", header=F, stringsAsFactors = F)$V1
 
+total.activity <- c(train.activity, test.activity)
+total.activity.descr.labels <- activity.labels$V2[total.activity]
 
-#Appropriately labels the data set with descriptive variable names. 
+### Appropriately labels the data set with descriptive variable names. 
 
-
-
+names(mean_and_std_data) <- feature.labels[c(mean_idx, std_idx)]
+train.subject <- read.table("UCI HAR Dataset/train/subject_train.txt", header=F)$V1
+test.subject <- read.table("UCI HAR Dataset/test/subject_test.txt", header=F)$V1
+total.subject <- c(train.subject, test.subject)
+data_all <- cbind(total.subject, total.activity.descr.labels, mean_and_std_data)
 
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+require(reshape2)
+data_melt <- melt(data_all, id.vars=c("total.subject", "total.activity.descr.labels"), measure.vars=colnames(data_all)[-(1:2)])
+mean_for_each_subject_and_activity <- dcast(data_melt, total.subject + total.activity.descr.labels ~ variable, mean)
+
